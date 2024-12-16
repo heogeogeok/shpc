739411946e074c1deed09aa3eab3d1195c39e2cf
juheehan@dbs.snu.ac.kr <juheehan@dbs.snu.ac.kr>
2024-12-17 02:06:21 +0900

    navie CUDA code

--- final-project/src/layer.cu
+++ final-project/src/layer.cu
@@ -2,17 +2,44 @@
 
 #define div(x, y) (((x) + (y) -1) / (y))
 
+/** SECTION: DEBUGGING **/
+#define DEBUG 0
+#if DEBUG == 1
+double dbg_start_time, dbg_ce_init, dbg_ce_final;
+#define DEBUG_PRINT(...) do { \
+  printf(__VA_ARGS__); \
+} while (0)
+#else
+#define DEBUG_PRINT(...)
+#endif
+
 /** SECTION: GPU manipulation **/
 #define NGPU    4
+static cudaStream_t streams[NGPU];
 
 /** SECTION: Kernels **/
-/* ReLU CUDA kernel */
-__global__ void krelu(float *inout, size_t N) {
-  size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-  if (i < N) {
-    inout[i] = inout[i] > 0 ? inout[i] : 0;
+/* Embedding CUDA kernel */
+__global__ void kembedding(const int *in, const float *w, float *out, size_t s, size_t H, size_t embedding_dim) {
+
+  int i = blockIdx.y * blockDim.y + threadIdx.y;
+  int j = blockIdx.x * blockDim.x + threadIdx.x; 
+
+  if (i < s && j < H) { 
+    out[i * H + j] = w[in[i] * H + j];
   }
 }
+
+/* Permute CUDA kernel */
+__global__ void kpermute(const float *in, float *out, size_t s, size_t H) {
+
+  int i = blockIdx.y * blockDim.y + threadIdx.y;  
+  int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (i < s && j < H) {
+    out[j * s + i] = in[i * H + j]; 
+  }
+}
+
 
 /* Conv1D CUDA kernel */
 __global__ void kconv1d(float *in, float *w, float *b, float *out, 
@@ -34,6 +61,39 @@ __global__ void kconv1d(float *in, float *w, float *b, float *out,
     }
 }
 
+/* GetMax CUDA kernel */
+__global__ void kgetmax(const float *in, float *out, size_t s, size_t C) {
+  
+  int i = blockIdx.x * blockDim.x + threadIdx.x;  
+
+  if (i < C) {  
+    float max_val = in[i * s]; 
+    
+    for (size_t j = 1; j < s; j++) {
+      max_val = max(max_val, in[i * s + j]);
+    }
+
+    out[i] = max_val;
+  }
+}
+
+/* Concat CUDA kernel */
+__global__ void kconcat(const float *in1, const float *in2, const float *in3, const float *in4, 
+                              float *out, size_t N1, size_t N2, size_t N3, size_t N4) {
+  
+  size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < N1) {
+    out[idx] = in1[idx];
+  } else if (idx < N1 + N2) {
+    out[idx] = in2[idx - N1];
+  } else if (idx < N1 + N2 + N3) {
+    out[idx] = in3[idx - N1 - N2];
+  } else if (idx < N1 + N2 + N3 + N4) {
+    out[idx] = in4[idx - N1 - N2 - N3];
+  }
+}
+
 /* Linear CUDA kernel */
 __global__ void klinear(float *in, float *w, float *b, float *out, int N, int M, bool relu) {
    
@@ -50,8 +110,6 @@ __global__ void klinear(float *in, float *w, float *b, float *out, int N, int M,
     if (relu) {
         val = fmaxf(val, 0.0f);
     }
-    
-
     out[i] = val;
     }
 }
@@ -63,16 +121,34 @@ __global__ void klinear(float *in, float *w, float *b, float *out, int N, int M,
  * 's' is the sequence length
  * 'H' is the embedding dimension
  */
+
 void Embedding(int *in, Tensor* w, Tensor *out) {
   size_t s = out->shape[0];
   size_t H = out->shape[1];
 
-  #pragma omp parallel for
-  for (size_t i = 0; i < s; i++) {
-    for (size_t j = 0; j < H; j++) {
-      out->buf[i * H + j] = w->buf[in[i] * H + j];
-    }
-  }
+  int *d_in;
+  float *d_w, *d_out;
+  size_t in_size = s * sizeof(int);
+  size_t w_size = w->shape[0] * w->shape[1] * sizeof(float);
+  size_t out_size = s * H * sizeof(float);
+
+  CHECK_CUDA(cudaMalloc(&d_in, in_size));
+  CHECK_CUDA(cudaMalloc(&d_w, w_size));
+  CHECK_CUDA(cudaMalloc(&d_out, out_size));
+
+  CHECK_CUDA(cudaMemcpy(d_in, in, in_size, cudaMemcpyHostToDevice));
+  CHECK_CUDA(cudaMemcpy(d_w, w->buf, w_size, cudaMemcpyHostToDevice));
+
+  dim3 blockDim(16, 16);
+  dim3 gridDim((H + blockDim.x - 1) / blockDim.x, (s + blockDim.y - 1) / blockDim.y);
+
+  kembedding<<<gridDim, blockDim>>>(d_in, d_w, d_out, s, H, w->shape[1]);
+
+  CHECK_CUDA(cudaMemcpy(out->buf, d_out, out_size, cudaMemcpyDeviceToHost));
+
+  CHECK_CUDA(cudaFree(d_in));
+  CHECK_CUDA(cudaFree(d_w));
+  CHECK_CUDA(cudaFree(d_out));
 }
 
 /* Permute
@@ -80,15 +156,27 @@ void Embedding(int *in, Tensor* w, Tensor *out) {
  * @param [out] out: [N, M]
  */
 void Permute(Tensor *in, Tensor *out) {
-  size_t s = in->shape[0];
+  size_t s = in->shape[0];  
   size_t H = in->shape[1];
 
-  // #pragma omp parallel for
-  for (size_t i = 0; i < s; i++) {
-    for (size_t j = 0; j < H; j++) {
-      out->buf[j * s + i] = in->buf[i * H + j];
-    }
-  }
+  float *d_in, *d_out;
+  size_t in_size = s * H * sizeof(float);
+  size_t out_size = s * H * sizeof(float);
+
+  CHECK_CUDA(cudaMalloc(&d_in, in_size));
+  CHECK_CUDA(cudaMalloc(&d_out, out_size));
+
+  CHECK_CUDA(cudaMemcpy(d_in, in->buf, in_size, cudaMemcpyHostToDevice));
+
+  dim3 blockDim(16, 16);
+  dim3 gridDim((H + blockDim.x - 1) / blockDim.x, (s + blockDim.y - 1) / blockDim.y);
+
+  kpermute<<<gridDim, blockDim>>>(d_in, d_out, s, H);
+
+  CHECK_CUDA(cudaMemcpy(out->buf, d_out, out_size, cudaMemcpyDeviceToHost));
+
+  CHECK_CUDA(cudaFree(d_in));
+  CHECK_CUDA(cudaFree(d_out));
 }
 
 /* Conv1D 
@@ -125,6 +213,11 @@ void Conv1D(Tensor *in, Tensor *w, Tensor *b, Tensor *out){
   size_t size_b = b->num_elem() * sizeof(float);
   size_t size_out = out->num_elem() * sizeof(float);
 
+  for (int i = 0; i < NGPU; ++i) {
+    CHECK_CUDA(cudaSetDevice(i));
+    CHECK_CUDA(cudaStreamCreate(&streams[i]));
+  }
+
   CHECK_CUDA(cudaMalloc(&d_in, size_in));
   CHECK_CUDA(cudaMalloc(&d_w, size_w));
   CHECK_CUDA(cudaMalloc(&d_b, size_b));
@@ -158,18 +251,28 @@ void Conv1D(Tensor *in, Tensor *w, Tensor *b, Tensor *out){
  * 'C' is the channel size
  * 's' is the sequence length
  */
-void GetMax(Tensor *in, Tensor *out) {
+void GetMax(Tensor *in, Tensor *out){
   size_t C = in->shape[0];
   size_t s = in->shape[1];
 
-  #pragma omp parallel for
-  for (size_t i = 0; i < C; i++) {
-    out->buf[i] = in->buf[i * s];
-    for (size_t j = 1; j < s; j++) {
-      out->buf[i] = in->buf[i * s + j] > out->buf[i] ? 
-        in->buf[i * s + j] : out->buf[i];
-    }
-  }
+  float *d_in, *d_out;
+  size_t in_size = C * s * sizeof(float);
+  size_t out_size = C * sizeof(float);
+
+  cudaMalloc(&d_in, in_size);
+  cudaMalloc(&d_out, out_size);
+
+  cudaMemcpy(d_in, in->buf, in_size, cudaMemcpyHostToDevice);
+
+  dim3 blockDim(256);  
+  dim3 gridDim((C + blockDim.x - 1) / blockDim.x);
+
+  kgetmax<<<gridDim, blockDim>>>(d_in, d_out, s, C);
+
+  cudaMemcpy(out->buf, d_out, out_size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_in);
+  cudaFree(d_out);
 }
 
 /* Concat
@@ -182,23 +285,37 @@ void GetMax(Tensor *in, Tensor *out) {
  */
 void Concat(Tensor *in1, Tensor *in2, Tensor *in3, Tensor *in4, 
             Tensor *out) {
+  
   size_t N1 = in1->shape[0];
   size_t N2 = in2->shape[0];
   size_t N3 = in3->shape[0];
   size_t N4 = in4->shape[0];
 
-  for (size_t i = 0; i < N1; i++) {
-    out->buf[i] = in1->buf[i];
-  }
-  for (size_t i = 0; i < N2; i++) {
-    out->buf[N1 + i] = in2->buf[i];
-  }
-  for (size_t i = 0; i < N3; i++) {
-    out->buf[N1 + N2 + i] = in3->buf[i];
-  }
-  for (size_t i = 0; i < N4; i++) {
-    out->buf[N1 + N2 + N3 + i] = in4->buf[i];
-  }
+  float *d_in1, *d_in2, *d_in3, *d_in4, *d_out;
+  size_t out_size = (N1 + N2 + N3 + N4) * sizeof(float);
+
+  CHECK_CUDA(cudaMalloc(&d_in1, N1 * sizeof(float)));
+  CHECK_CUDA(cudaMalloc(&d_in2, N2 * sizeof(float)));
+  CHECK_CUDA(cudaMalloc(&d_in3, N3 * sizeof(float)));
+  CHECK_CUDA(cudaMalloc(&d_in4, N4 * sizeof(float)));
+  CHECK_CUDA(cudaMalloc(&d_out, out_size));
+
+  CHECK_CUDA(cudaMemcpy(d_in1, in1->buf, N1 * sizeof(float), cudaMemcpyHostToDevice));
+  CHECK_CUDA(cudaMemcpy(d_in2, in2->buf, N2 * sizeof(float), cudaMemcpyHostToDevice));
+  CHECK_CUDA(cudaMemcpy(d_in3, in3->buf, N3 * sizeof(float), cudaMemcpyHostToDevice));
+  CHECK_CUDA(cudaMemcpy(d_in4, in4->buf, N4 * sizeof(float), cudaMemcpyHostToDevice));
+
+  dim3 blockDim(256);
+  dim3 gridDim((N1 + N2 + N3 + N4 + blockDim.x - 1) / blockDim.x);
+
+  kconcat<<<gridDim, blockDim>>>(d_in1, d_in2, d_in3, d_in4, d_out, N1, N2, N3, N4);
+
+  CHECK_CUDA(cudaMemcpy(out->buf, d_out, out_size, cudaMemcpyDeviceToHost));
+  CHECK_CUDA(cudaFree(d_in1));
+  CHECK_CUDA(cudaFree(d_in2));
+  CHECK_CUDA(cudaFree(d_in3));
+  CHECK_CUDA(cudaFree(d_in4));
+  CHECK_CUDA(cudaFree(d_out));
 }
 
 /* Linear 
@@ -210,6 +327,7 @@ void Concat(Tensor *in1, Tensor *in2, Tensor *in3, Tensor *in4,
  * 'M' is the output feature size
  */
 void Linear(Tensor *in, Tensor *w, Tensor *b, Tensor *out) {
+    
     float *d_in, *d_w, *d_b, *d_out;
     size_t size_in = in->num_elem() * sizeof(float);
     size_t size_w = w->num_elem() * sizeof(float);
@@ -239,6 +357,7 @@ void Linear(Tensor *in, Tensor *w, Tensor *b, Tensor *out) {
 }
 
 void Linear_ReLU(Tensor *in, Tensor *w, Tensor *b, Tensor *out){
+    
     float *d_in, *d_w, *d_b, *d_out;
     size_t size_in = in->num_elem() * sizeof(float);
     size_t size_w = w->num_elem() * sizeof(float);
